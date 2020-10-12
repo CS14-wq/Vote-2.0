54af3463497cd1e3d6384e66f3677b84e53c3ae0
CS14 <csboss11@outlook.com>
2020-10-12 14:22:30 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,40 @@
+ import java.util.scanner;
+  public class Scannertut {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+      
+      if ("") {
+      
+      } else {
+      
+      }
+      
+      System.out.println("What is your first and last name?") ;
+      String name = scan.next();
+
+      System.out.println("What is your age and home state?"); Int age = scan.nextInt() ;
+      
+      System.out.println("Who are you voting for?");
+      String vote = scan.nextInt() ;
+      String vote += scan.nextLine();
+      System.out.println("Thank you"+ name "for voting" )
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  }
+}
