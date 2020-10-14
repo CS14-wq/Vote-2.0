a6f9cc14b183c693321b3c1e1223829c8fb8da58
CS14 <csboss11@outlook.com>
2020-10-14 13:10:33 +0000

    Added && and some more printlns

--- Main.java
+++ Main.java
@@ -1,40 +1,24 @@
- import java.util.scanner;
-  public class Scannertut {
+ import java.util.Scanner;
+  public class Main {
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
-      
-      if ("") {
-      
-      } else {
-      
-      }
-      
-      System.out.println("What is your first and last name?") ;
-      String name = scan.next();
 
-      System.out.println("What is your age and home state?"); Int age = scan.nextInt() ;
+    System.out.println("Are you ready to vote -- true or false?");
+    boolean ready = scan.nextBoolean();
+
+    System.out.println("What is your first name?") ;
+    String name = scan.next();
+
+    System.out.println("What is your age?"); 
+    int age = scan.nextInt();
       
-      System.out.println("Who are you voting for?");
-      String vote = scan.nextInt() ;
-      String vote += scan.nextLine();
-      System.out.println("Thank you"+ name "for voting" )
+    if (age >= 18 && ready) {
+      System.out.println("Welcome to the voting poles!");
+    } else {
+      System.out.println("Sorry your are not eligible");
+    }
   
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
+      
+    System.out.println("Thank you "+name+" for voting" ) ;
   }
 }
